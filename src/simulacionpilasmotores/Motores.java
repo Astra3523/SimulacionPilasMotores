c44c4b6029e4647f381173341c7374473ed5f86c
Saul <overcipress103@gmail.com>
2025-10-03 19:17:33 -0600

    Primer Commit, solo añadi la libreria e hice la clase motor

---
+++ src/simulacionpilasmotores/Motores.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simulacionpilasmotores;
+
+/**
+ *
+ * @author Astra
+ */
+public class Motores {
+    
+    String marca;
+    String modelo;
+    int potencia;
+
+    public Motores() {
+    }
+
+    public Motores(String marca, String modelo, int potencia) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.potencia = potencia;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Motores{" + "marca=" + marca + ", modelo=" + modelo + ", potencia=" + potencia + '}';
+    }
+    
+    
+    
+}
+    
+    
+
