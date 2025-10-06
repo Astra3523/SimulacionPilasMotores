5fa454cf636237f22628dad10a4e07dc1b5a97c9
Saul <overcipress103@gmail.com>
2025-10-05 22:29:55 -0600

    Hicimos la logica general del programa

--- src/simulacionpilasmotores/SimulacionPilasMotores.java
+++ src/simulacionpilasmotores/SimulacionPilasMotores.java
@@ -19,6 +19,7 @@ public class SimulacionPilasMotores {
         TdaPila<Motores> pila2 = new TdaPila<>(7);
         TdaPila<Motores> almacenEntrada = new TdaPila<>(100);
         TdaPila<Motores> almacenSalida = new TdaPila<>(100);
+        Motores c;
         Scanner leer = new Scanner(System.in);
         int opc;
         
@@ -26,7 +27,13 @@ public class SimulacionPilasMotores {
             System.out.println("TALLER MECANICO AUTOMOTRIZ");
             System.out.println("1.- Agregar motor a almacen de entrada");
             System.out.println("2.- Acerca de...");
-            System.out.println("3.- Salir");
+            System.out.println("3.- Mover de almacenEntrada a pila 1");
+            System.out.println("4.- Mover de almacenEntrada a pila 2");
+            System.out.println("5.- Mover de pila 1 a pila 2");
+            System.out.println("6.- Mover de pila 2 a pila 1");
+            System.out.println("7.- Mover de pila 1 a almacenSalida");
+            System.out.println("8.- Mover de pila 2 a almacenSalida");
+            System.out.println("9.- Salir");
             System.out.print("Seleccione una opcion: ");
             opc = leer.nextInt();
             leer.nextLine();
@@ -42,7 +49,8 @@ public class SimulacionPilasMotores {
                     Motores motor1 = new Motores(marca, modelo, potencia); 
                     almacenEntrada.push(motor1);
                     System.out.println("Motor agregado a almacen de entrada "+motor1);
-                break;    
+                break;
+                
                 case 2:
                     System.out.println("Acerca de:");
                     System.out.println("Simulacion de motores");
@@ -53,14 +61,162 @@ public class SimulacionPilasMotores {
                     System.out.println("Reyes Cázares Aldo Emanuel");    
                     System.out.println();
                 break;
-                case 3:
+                
+                case 9:
                     System.out.println("Saliendo...");
+                    break;
+                case 3:
+                    
+                    if(almacenEntrada.isVacia()){
+                        System.out.println("El almacen entrada esta vacio");
+                        break;
+                    }
+                    else if(pila1.isLlena()){
+                        System.out.println("La pila 1 esta llena");
+                        break;
+                    }
+                    
+                    try{
+                        c = almacenEntrada.peek();
+                        almacenEntrada.pop();
+                        pila1.push(c);
+                        System.out.println("El motor se cambio a la pila 1 exitosamente");
+                        leer.nextLine();
+                        leer.nextLine();
+                        
+                    }catch(IllegalStateException e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 4:
+                    
+                        if(almacenEntrada.isVacia()){
+                        System.out.println("El almacen entrada esta vacio");
+                        break;
+                    }
+                    else if(pila2.isLlena()){
+                        System.out.println("La pila 2 esta llena");
+                        break;
+                    }
+                    
+                    try{
+                        c = almacenEntrada.peek();
+                        almacenEntrada.pop();
+                        pila2.push(c);
+                        System.out.println("El motor se cambio a la pila 2 exitosamente");
+                        leer.nextLine();
+                        leer.nextLine();
+                        
+                    }catch(IllegalStateException e){
+                        System.out.println(e.getMessage());
+                    }
+                    
+                    break;
+                case 5:
+                        if(pila1.isVacia()){
+                        System.out.println("La pila 1 vacio");
+                        break;
+                    }
+                    else if(pila2.isLlena()){
+                        System.out.println("La pila 2 esta llena");
+                        break;
+                    }
+                    
+                    try{
+                        c = pila1.peek();
+                        pila1.pop();
+                        pila2.push(c);
+                        System.out.println("El motor se cambio de pila 1 a pila 2");
+                        System.out.println(c);
+                        leer.nextLine();
+                        leer.nextLine();
+                        
+                    }catch(IllegalStateException e){
+                        System.out.println(e.getMessage());
+                    }
+                    
+                    break;
+                    
+                case 6:
+                    
+                        if(pila1.isVacia()){
+                        System.out.println("La pila 1 vacio");
+                        break;
+                    }
+                    else if(pila2.isLlena()){
+                        System.out.println("La pila 2 esta llena");
+                        break;
+                    }
+                    
+                    try{
+                        c = pila2.peek();
+                        pila2.pop();
+                        pila1.push(c);
+                        System.out.println("El motor se cambio de pila 2 a pila 1");
+                        System.out.println(c);
+                        leer.nextLine();
+                        leer.nextLine();
+                        
+                    }catch(IllegalStateException e){
+                        System.out.println(e.getMessage());
+                    }
+                    
+                    break;
+                    
+                case 7:
+                    if(pila1.isVacia()){
+                        System.out.println("La pila 1 esta vacia");
+                        break;
+                    }
+                    else if(almacenSalida.isLlena()){
+                        System.out.println("El almacen de salida esta lleno");
+                        break;
+                    }
+                    
+                    try{
+                        c = pila1.peek();
+                        pila1.pop();
+                        almacenSalida.push(c);
+                        System.out.println("El motor se envio al almacen de salida");
+                        System.out.println(c);
+                        leer.nextLine();
+                        leer.nextLine();
+                        
+                    }catch(IllegalStateException e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                    
+                case 8:
+                        if(pila2.isVacia()){
+                        System.out.println("La pila 2 esta vacia");
+                        break;
+                    }
+                    else if(almacenSalida.isLlena()){
+                        System.out.println("El almacen de salida esta lleno");
+                        break;
+                    }
+                    
+                    try{
+                        c = pila2.peek();
+                        pila2.pop();
+                        almacenSalida.push(c);
+                        System.out.println("El motor se envio al almacen de salida");
+                        System.out.println(c);
+                        leer.nextLine();
+                        leer.nextLine();
+                        
+                    }catch(IllegalStateException e){
+                        System.out.println(e.getMessage());
+                    }
+                    
+                    
                     break;
                 default:
                     System.out.println("Seleccione una opcion valida");
             }
         
-        }while(opc != 3);
+        }while(opc != 9);
         
     }
     
